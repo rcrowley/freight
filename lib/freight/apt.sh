87b78bbaa4bae4b27c044d2dacae66fa33108181
Richard Crowley <r@rcrowley.org>
2011-07-17 00:40:57 +0000

    Extract the entire version, not just the epoch.
    In cases where there was an epoch, only it was returned.  This also goes for any field that contains a : character.

--- lib/freight/apt.sh
+++ lib/freight/apt.sh
@@ -1,6 +1,6 @@
 # Fetch the given field from the package's control file.
 apt_info() {
-	egrep -i "^$2:" "$1" | cut -d: -f2 | cut -c2-
+	egrep -i "^$2:" "$1" | cut -d: -f2- | cut -c2-
 }
 
 # Print the package name from the given control file.
